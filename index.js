9e4dc5189cde4dfd8ba7efff7dc6de37470f65cd
tomas <tomasbeyene21@gmail.com>
2023-01-30 19:33:59 +0300

    class

--- index.js
+++ index.js
@@ -1,32 +1,32 @@
-//object litrals
-const circle ={
-    radius:1,
-    draw: function(){
-        console.log('draw');
-    }
-};
+// //object litrals
+// const circle ={
+//     radius:1,
+//     draw: function(){
+//         console.log('draw');
+//     }
+// };
 
-const c1=circle.draw();
-console.log(c1)
-
-
+// const c1=circle.draw();
+// console.log(c1)
 
 
 
 
 
-//factory functon
-function createCircle(radius){
-    return{
-        radius:radius,
-       draw: function(){
-        console.log('draw');
-    }
-    };
-} 
 
-const c2=createCircle(4)
-console.log(c2)
+
+// //factory functon
+// function createCircle(radius){
+//     return{
+//         radius:radius,
+//        draw: function(){
+//         console.log('draw');
+//     }
+//     };
+// } 
+
+// const c2=createCircle(4)
+// console.log(c2)
 
 
 
@@ -59,45 +59,43 @@ function Circle(radius){
     }); 
 } 
 
+console.log(Circle.prototype)
 
-c3.dlocation=2;//setter
-console.log(c3.dlocation)//getter
-
-
-const c3=new Circle(88)
-//or
-const c3=Circle.call({},88);
-const c3=Circle.apply({},[88,4,3]); //it it's array
-
-c3.location=9;//add ppt
-console.log(c3)
-
-delete c3.location;//delete ppt
-console.log(c3)
-
-//list all properties
-for (let key in c3){
-    console.log(key,c3[key]);
-}
-
-//or 
-
-console.log(Object.keys(c3))
+// c3.dlocation=2;//setter
+// console.log(c3.dlocation)//getter
 
 
+// const c3=new Circle(88)
+// //or
+// const c3=Circle.call({},88);
+// const c3=Circle.apply({},[88,4,3]); //it it's array
 
+// c3.location=9;//add ppt
+// console.log(c3)
 
-//another wayto define an object/function
-const Circle= new Function('radius',`
-     this.radius=radius,
-     this.draw= function(){
-     console.log('draw');}`
-     );
-const c5=Function(1);
-console.log(c5);
+// delete c3.location;//delete ppt
+// console.log(c3)
+
+// //list all properties
+// for (let key in c3){
+//     console.log(key,c3[key]);
+// }
+
+// //or 
+
+// console.log(Object.keys(c3))
 
 
 
+
+// //another wayto define an object/function
+// const Circle= new Function('radius',`
+//      this.radius=radius,
+//      this.draw= function(){
+//      console.log('draw');}`
+//      );
+// const c5=Function(1);
+// console.log(c5);
 
 
 
@@ -105,18 +103,21 @@ console.log(c5);
 
 
 
-//reference types
-let x1={a:3};
-let y1=x;
-x1.a=9
-console.log(x1.a,y1.a)
 
 
-//value types
-let x2=3;
-let y2=x2;
-x2=9
-console.log(x2,y2)
+
+// //reference types
+// let x1={a:3};
+// let y1=x;
+// x1.a=9
+// console.log(x1.a,y1.a)
+
+
+// //value types
+// let x2=3;
+// let y2=x2;
+// x2=9
+// console.log(x2,y2)
 
 
 
