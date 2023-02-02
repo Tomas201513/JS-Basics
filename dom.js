e624b066267b9b846c71759901f9390237d24c5e
Tomas Beyene <94804547+Tomas201513@users.noreply.github.com>
2023-02-02 16:42:32 +0300

    DOM1

--- dom.js
+++ dom.js
@@ -8,13 +8,26 @@
 // console.log(z)
 
 
-// insert element
-const ul= document.querySelector('ul');
-console.log(ul);
-const li=document.createElement('li');
+// // insert element
+// const ul= document.querySelector('ul');
+// console.log(ul);
+// const li=document.createElement('li');
 
-// inserting element
-zz=ul.append(li);
-console.log(typeof li);
+// // inserting element
+// zz=ul.append(li);
+// console.log(typeof li);
+
+
+
+const div= document.querySelector('div');
+console.log(div);
 
 // modifing the txt
+const h4=document.createElement('h4')
+const zz=div.append(h4);
+h4.innerText='tom';
+console.log(div);
+
+// modifing the attribute
+
+h4.setAttribute('id','abc')
